c3553ce6361df9e1303daa2f2d77eb6bc6e0b76e
ximena <ximena.cordero@nearshorecode.com>
2020-01-15 00:13:19 -0400

    Added sigIn component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
+import SigInSignUpPage from './pages/signin-signup/signin-signup.component'
+
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
 
@@ -13,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SigInSignUpPage} />
       </Switch>      
     </div>
   );
---
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './signin-signup.styles.scss';
+import SignIn from '../../components/sign-in/signin.component'
+
+const SigInSignUpPage = () => (
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+    </div>
+)
+
+export default SigInSignUpPage
